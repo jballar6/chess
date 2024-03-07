845845f5d8c69e02660c0fc88c588d6cf6ceab99
Jonathan Ballard <jonballard6@outlook.com>
2024-03-06 23:45:25 -0700

    Creating initial concept schema for database

--- server/src/main/java/dataAccess/MySQLDataAccess.java
+++ server/src/main/java/dataAccess/MySQLDataAccess.java
@@ -98,6 +98,28 @@ public class MySQLDataAccess implements DataAccess {
     }
 
     private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  users (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `name` varchar(256) NOT NULL,
+              `auth` int,
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(auth),
+              INDEX(name)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            
+            CREATE TABLE IF NOT EXISTS  games (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `name` varchar(256) NOT NULL,
+              `blackplayer` int,
+              `whiteplayer` int,
+              `observers` int,
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(type)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
     };
 
     private void configureDatabase() throws ResponseException {
