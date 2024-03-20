d13f1ef31ba286a07927f3502127a434dc48ae1c
Jonathan Ballard <jonballard6@outlook.com>
2024-03-19 22:52:03 -0600

    Renaming main files in client and server packages to avoid confusion

--- server/src/main/java/ServerMain.java
+++ server/src/main/java/ServerMain.java
@@ -1,7 +1,7 @@
 import chess.*;
 import server.Server;
 
-public class Main {
+public class ServerMain {
     public static void main(String[] args) {
         var server = new Server();
         server.run(8080);
